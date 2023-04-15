babd042337ecceca855f724964eedc920172fbad
rph3j <1piotr.wyrwas@gmail.com>
2023-04-15 14:57:12 +0200

    first commit

---
+++ ProjektIoTSdk.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using ProjektIoTSdk.Device;
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx.Client;
+using Opc.UaFx;
+
+// tworze i uzywam połączeniea
+string Key = "HostName=zajencia-IoT-23.azure-devices.net;DeviceId=Device01;SharedAccessKey=F5MFBFYHLsG8iAbKJUpe8iBg16JpW3DZvHMgEtj79jc=";
+using var deviceClient = DeviceClient.CreateFromConnectionString(Key, TransportType.Mqtt);
+
+// Odpalam deviceClient i tworze VirtualDevice
+await deviceClient.OpenAsync();
+var device = new VirtualDevice(deviceClient);
+Console.Write("Connection succesful!");
+Console.WriteLine();
+
+
+await device.SendMessages(1);
+
+    
+
+
+
+
+Console.WriteLine("Prase kay to continue...");
+Console.ReadLine();
---
+++ ProjektIoTSdk.Device/VirtualDevice.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Opc.UaFx.Client;
+using Opc.UaFx;
+using Newtonsoft.Json;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Azure.Amqp.Framing;
+
+namespace ProjektIoTSdk.Device
+{
+    public class VirtualDevice
+    {
+        private readonly DeviceClient deviceClient;
+
+        public VirtualDevice(DeviceClient deviceClient)
+        {
+            this.deviceClient = deviceClient;
+        }
+
+        public async Task SendMessages(int MachienNumber)
+        {
+            using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+            {
+                client.Connect();
+
+                var commands = new OpcReadNode[]
+                {
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/ProductionStatus", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/ProductionStatus"),
+
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/WorkorderId", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/WorkorderId"),
+
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/Temperature", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/Temperature"),
+
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/GoodCount", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/GoodCount"),
+
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/BadCount", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/BadCount"),
+
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/DeviceError", OpcAttribute.DisplayName),
+                    new OpcReadNode($"ns=2;s=Device {MachienNumber}/DeviceError"),
+                };
+
+                var job = client.ReadNodes(commands); // dane w tablicę
+
+                var d = "";
+                foreach (var item in job)
+                {
+                    d += ($"{item.Value}: {item.SourceTimestamp}");
+                }
+                var dataString = JsonConvert.SerializeObject(d);
+
+                Message eventMassage = new Message(Encoding.UTF8.GetBytes(dataString));
+                eventMassage.ContentType = MediaTypeNames.Application.Json;
+                eventMassage.ContentEncoding = "utf-8";
+
+                //Console.WriteLine($"\t{DateTime.Now.ToLocalTime()} > Device: {MachienNumber} send message, Data: [{dataString}]"); // wypis w konsoli że wysłało
+
+                await deviceClient.SendEventAsync(eventMassage); // oficjalne wysłanie
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
