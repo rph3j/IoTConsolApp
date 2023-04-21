ad4e91e71b44469aaeb16f2232210ebd5903c07a
rph3j <101104125+rph3j@users.noreply.github.com>
2023-04-21 21:38:41 +0200

    The config file was added!

--- ProjektIoTSdk.Console/Program.cs
+++ ProjektIoTSdk.Console/Program.cs
@@ -1,25 +1,48 @@
 ﻿using ProjektIoTSdk.Device;
 using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.IO;
 using Opc.UaFx.Client;
 using Opc.UaFx;
-
-// tworze i uzywam połączeniea
-string Key = "HostName=zajencia-IoT-23.azure-devices.net;DeviceId=Device01;SharedAccessKey=F5MFBFYHLsG8iAbKJUpe8iBg16JpW3DZvHMgEtj79jc=";
-using var deviceClient = DeviceClient.CreateFromConnectionString(Key, TransportType.Mqtt);
-
-// Odpalam deviceClient i tworze VirtualDevice
-await deviceClient.OpenAsync();
-var device = new VirtualDevice(deviceClient);
-Console.Write("Connection succesful!");
-Console.WriteLine();
+using Microsoft.Azure.Amqp.Framing;
+using Newtonsoft.Json.Linq;
 
 
-await device.SendMessages(1);
+string config;
+try
+{
+    #region Read Config file
+    using (var sr = new StreamReader("A:\\VisualRepo\\ProjektIoTSdk.Console\\Config.json"))
+    {
+        config = sr.ReadToEnd();
+    }
+    #endregion
 
-    
+    JObject jsonObject = JObject.Parse(config);
 
+    int counter = ((int)jsonObject["CountOfDevices"]);
+    for (int i = 1; i <= counter; i++)
+    {
+        #region Conection
 
+        string Key = jsonObject["conectionStrings"][i-1].ToString();
+        using var deviceClient = DeviceClient.CreateFromConnectionString(Key, TransportType.Mqtt);
+        await deviceClient.OpenAsync();
+        var device = new VirtualDevice(deviceClient);
 
+        Console.Write("Connection succesful!");
+        Console.WriteLine();
 
-Console.WriteLine("Prase kay to continue...");
-Console.ReadLine();
+        #endregion
+        await device.SendMessages(i);
+    }
+
+    Console.WriteLine("Prase kay to continue...");
+    Console.ReadLine();
+}
+catch (Exception e) 
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message); 
+}
+
